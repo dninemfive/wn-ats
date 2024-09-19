42389b52a6f8567af3a72662e3e366893d48b237
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-09-19 16:47:40 -0600

    weird bug

--- scripts/type_profile.py
+++ scripts/type_profile.py
@@ -1,7 +1,7 @@
 import os
 from typing import Any, Self
 from ndf_parse import Mod
-from ndf_parse.model import List, ListRow, Map, MapRow, MemberRow, Object
+from ndf_parse.model import List, ListRow, Map, MapRow, MemberRow, Object, Template
 import ndf_parse.model.abc as abc
 import re
 
@@ -25,7 +25,7 @@ class Type(object):
         self.dict: dict[str, TypeAnnotation] = {}
         self.found_in_files: set[str] = set([current_file])
 
-    def update(self: Self, member_name: str, type: str, current_file: str) -> None:
+    def update(self: Self, member_name: str, type: str) -> None:
         if member_name in self.dict:
             self.dict[member_name].add(type)
         else:
@@ -35,16 +35,41 @@ class Type(object):
         for k, v in self.dict.items():
             yield (k, v)
 
+    @property
+    def class_string(self: Self) -> str:
+        def generate_rows():
+            yield 'from dataclasses import dataclass'
+            yield ''
+            yield '@dataclass'
+            yield f'class {self.name}(object):'
+            for k, v in self.members:
+                yield f'{k}: {str(v)}'
+        return "\n".join(generate_rows())
+    
+    def write(self: Self) -> None:
+        with open(f'types/{self.name}.py', 'w') as file:
+            file.write(self.class_string)
+
 class TypeSet(object):
     def __init__(self: Self):
         self.types: dict[str, Type] = {}
 
-    def add(self: Self, type_name: str):
+    def add(self: Self, obj: Object | Template | str, current_file: str) -> Type:
+        type_name: str
+        if isinstance(obj, (Object, Template)):
+            type_name = obj.type
+        else:
+            type_name = obj
         if type_name not in self.types:
-            self.types[type_name] = Type(type_name)
+            self.types[type_name] = Type(type_name, current_file)
+        return self.types[type_name]
 
     def get(self: Self, type_name: str) -> Type:
         return self.types[type_name]
+    
+    def write_all(self: Self) -> None:
+        for _, t in self.types.items():
+            t.write()
     
 def get_all_ndf_files(mod: Mod, root_folder: str): # -> Generator[tuple[str, List]]
     all_ndf: set[str] = set()
@@ -77,6 +102,9 @@ def strip_type(type: str | type) -> str:
 
 PRIMITIVE_TYPES = [int, float] #, bool : apparently bool(any_str) is a bool??
 
+def is_primitive(val: Any) -> bool:
+    return not isinstance(val, (abc.List, abc.Row))
+
 def determine_type(val: Any) -> str:
     for t in PRIMITIVE_TYPES:
         if is_type(val, t):
@@ -91,40 +119,29 @@ def determine_type(val: Any) -> str:
         return f'{strip_type(type(val))}[{determine_types_in_list(val)}]'
     return "str" # TODO: refptr determination?
 
-def profile_members(obj: Object) -> dict[str, TypeAnnotation]:
-    result: dict[str, TypeAnnotation]
-    member: MemberRow
-    for member in obj:
-        pass
+def profile(object: Object | abc.Row | abc.List, global_types: TypeSet, current_file: str) -> None:
+    if is_primitive(object):
+        return
+    if isinstance(object, Object):
+        obj_type = global_types.add(object, current_file)
+        for member in object:
+            obj_type.update(member.member, determine_type(member.value))
+    elif isinstance(object, (ListRow, MapRow, MemberRow)):
+        profile(object.value, global_types, current_file)
+    else:
+        for item in object:
+            profile(item, global_types, current_file)
 
-def generate_class_file(type_name: str, members: dict[str, TypeAnnotation]) -> None:
-    def generate_rows():
-        yield 'from dataclasses import dataclass'
-        yield ''
-        yield '@dataclass'
-        yield f'class {type_name}(object):'
-        for k, v in members:
-            yield f'{k}: {str(v)}'
-    with open(f'types/{type_name}.py', 'w') as file:
-        file.write('\n'.join(generate_rows()))
-
-def list_type(list: List) -> TypeAnnotation:
-    pass
-
-seen_types = TypeSet()
 
 MOD_PATH = rf'C:\Program Files (x86)\Steam\steamapps\common\WARNO\Mods\default'
 
 mod = Mod(MOD_PATH, MOD_PATH)
+global_types = TypeSet()
 
-l = mod.edit(rf"GameData\Generated\Gameplay\Decks\Divisions.ndf", False).current_tree
-
-try:
-    for current_dir, _, filenames in os.walk(MOD_PATH):
-        for filename in filenames:
-            filename = os.path.relpath(os.path.join(current_dir, filename), MOD_PATH)
-            if os.path.splitext(filename)[1] == '.ndf':
-                tree = mod.edit(filename, False, False).current_tree
-                print(filename, determine_type(tree))
-except:
-    pass
+for current_dir, _, filenames in os.walk(MOD_PATH):
+    for filename in filenames:
+        filename = os.path.relpath(os.path.join(current_dir, filename), MOD_PATH)
+        if os.path.splitext(filename)[1] == '.ndf':
+            tree = mod.edit(filename, False, False).current_tree
+            profile(tree, global_types, filename)
+global_types.write_all()
