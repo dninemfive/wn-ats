a2a4a43e61fd1ef76d863f2de616fc88c96f6e48
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-09-25 18:29:36 -0600

    move things into their own folders; expand operator analysis

--- scripts/sound_operators/sound_operators.py
+++ scripts/sound_operators/sound_operators.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import os
 from time import time_ns
 
@@ -10,15 +11,20 @@ GENERATED_PATH = MOD_PATH # rf'{MOD_PATH}\GameData\Generated'
 mod = Mod(MOD_PATH, MOD_PATH)
 
 def time_since(start: int) -> str:
-    return f'{(time_ns() - start) / 1e9:.3f}s'.rjust(9)
+    return f'{(time_ns() - start) / 1e9:.3f}s'
 
 program_start = time_ns()
-unique_operators: set[str] = set()
+operators: defaultdict[str, set[str]] = defaultdict(lambda: set())
 with mod.edit('GameData/Generated/Gameplay/Gfx/Infanterie/GeneratedDepictionInfantry.ndf') as file:
     for row in file:
         value = row.value
         if isinstance(value, Object) and value.type == 'TemplateInfantryDepictionSquad':
-            unique_operators.add(value.by_member('SoundOperator').value)
+            operators[value.by_member('SoundOperator').value].add(" ".join(row.namespace.split('_')[1:]))
+result: list[str] = []
+for k in sorted(operators.keys()):
+    result.append(k)
+    for item in sorted(operators[k]):
+        result.append(f'\t{item}')
 with open('sound_operators.txt', 'w') as file:
-    file.write('\n'.join(sorted(unique_operators)))
-print(f"Found {len(unique_operators)} sound operators in", time_since(program_start))
+    file.write('\n'.join(result))
+print(f"Found {len(operators)} sound operators in", time_since(program_start))
