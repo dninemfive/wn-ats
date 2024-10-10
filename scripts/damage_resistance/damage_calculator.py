3f45159431606c5430dd6685b5f3d58bf9ca5b2f
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-10-09 23:13:10 -0600

    further damage calculator work

--- scripts/damage_resistance/damage_calculator.py
+++ scripts/damage_resistance/damage_calculator.py
@@ -2,12 +2,37 @@ from collections import Counter, defaultdict
 import os
 import sys
 from time import time_ns
-from typing import Any, Iterable, Literal
+from typing import Any, Iterable, Literal, Self
 
 from ndf_parse import Mod
 from ndf_parse.model import List, Object, Template
 
 FamilyDefinition = tuple[str, int]
+FamilyType = Literal['Resistance', 'Damage']
+
+class FamilyDefinitionList(object):
+    def __init__(self: Self, data: Object, type: FamilyType):
+        self.data: list[FamilyDefinition] = self.load_family(data)
+        self.type = type
+
+    def load_family(self: Self, data: Object) -> list[FamilyDefinition]:
+        family_list: List = data.by_member(f'{self.type}FamilyDefinitionList').value
+        result: list[FamilyDefinition] = []
+        for row in family_list:
+            family_def: Object = row.value
+            result.append((family_def.by_member('Family').value, int(family_def.by_member('MaxIndex').value)))
+        return result
+    
+    def get_index(self: Self, family: str, family_index: int) -> int:
+        family = ensure_startswith(family, f'{self.type}Family_{family}')
+        result: int = 0
+        for name, maxIndex in self.data:
+            if name == family:
+                if family_index >= maxIndex:
+                    raise Exception(f'Specified index {family_index} is greater than the MaxIndex of specified family {family}!')
+                return result + family_index
+            result += maxIndex
+        raise Exception(f"Didn't find family {family} in specified family list!")
 
 MOD_PATH = rf'C:\Program Files (x86)\Steam\steamapps\common\WARNO\Mods\default'
 GENERATED_PATH = MOD_PATH # rf'{MOD_PATH}\GameData\Generated'
@@ -42,36 +67,20 @@ def load_vals(data: Object) -> list[list[float]]:
         result.append(r)
     return result
 
-def load_family(data: Object, type: Literal['Resistance', 'Damage']) -> list[FamilyDefinition]:
-    family_list: List = data.by_member(f'{type}FamilyDefinitionList').value
-    result: list[FamilyDefinition] = []
-    for row in family_list:
-        family_def: Object = row.value
-        result.append((family_def.by_member('Family').value, int(family_def.by_member('MaxIndex').value)))
-    return result
-
-def get_index(families: list[FamilyDefinition], family: str, type: Literal['Resistance', 'Damage'], family_index: int) -> int:
-    family = ensure_startswith(family, f'{type}Family_{family}')
-    result: int = 0
-    for name, maxIndex in families:
-        if name == family:
-            if family_index >= maxIndex:
-                raise Exception(f'Specified index {family_index} is greater than the MaxIndex of specified family {family}!')
-            return result + family_index
-        result += maxIndex
-    raise Exception(f"Didn't find family {family} in specified family list!")
-
 with mod.edit('GameData/Generated/Gameplay/Gfx/DamageResistance.ndf') as file:
     print(time_since(program_start))
     damage_resistance: Object = file[0].value
     vals = load_vals(damage_resistance)
-    resistances = load_family(damage_resistance, 'Resistance')
-    damages = load_family(damage_resistance, 'Damage')
+    resistances = FamilyDefinitionList(damage_resistance, 'Resistance')
+    damages = FamilyDefinitionList(damage_resistance, 'Damage')
 
     col_index = get_index(resistances, sys.argv[3], 'Resistance', int(sys.argv[4]))
     row_index = get_index(damages, sys.argv[1], 'Damage', int(sys.argv[2]))
 
-    print(f'Calculated damage suffered by target {sys.argv[3]} {sys.argv[4]} by incoming damage {sys.argv[3]}')
+    print(f'Calculated damage suffered by target {sys.argv[3]} {sys.argv[4]} by incoming damage {sys.argv[1]} {sys.argv[2]} is {}')
     
 
 print(time_since(program_start))
+
+if __name__ == '__main__':
+    pass
