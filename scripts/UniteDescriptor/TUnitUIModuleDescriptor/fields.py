3212e84ecaaf1a75ec7ffef50e7576ed350d6646
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-10-10 13:38:57 -0600

    further profile TUnitUIModuleDescriptor

--- scripts/UniteDescriptor/TUnitUIModuleDescriptor/fields.py
+++ scripts/UniteDescriptor/TUnitUIModuleDescriptor/fields.py
@@ -1,6 +1,7 @@
 from collections import Counter, defaultdict
 import os
 from time import time_ns
+from tokenize import maybe
 from typing import Any, Iterable
 
 from ndf_parse import Mod
@@ -17,6 +18,30 @@ def time_since(start: int) -> str:
     return f'{(time_ns() - start) / 1e9:.3f}s'
 
 program_start = time_ns()
+
+def try_unquote(s: str) -> str:
+    if s.startswith("'") or s.startswith('"'):
+        s = s[1:]
+    if s.endswith("'") or s.endswith('"'):
+        s = s[:-1]
+    return s
+
+def find_common_prefix(strs: Iterable[str]) -> str | None:
+    strs = [try_unquote(s) for s in strs]
+    maybe_prefix = strs[0]
+    for s in strs[1:]:
+        if len(maybe_prefix) < 1:
+            break
+        while not s.startswith(maybe_prefix):
+            maybe_prefix = maybe_prefix[:-1]
+    return maybe_prefix if len(maybe_prefix) > 0 else None
+
+def strip_prefix(s: str, prefix: str) -> str:
+    s = try_unquote(s)
+    if s.startswith(prefix):
+        return s[len(prefix):]
+    return s
+        
 
 fields: dict[str, set[str]] = {x: set() for x in ['UnitRole', 'InfoPanelConfigurationToken', 'MenuIconTexture', 'CountryTexture', 'TypeStrategicCount']}
 
@@ -35,4 +60,9 @@ with mod.edit('GameData/Generated/Gameplay/Gfx/UniteDescriptor.ndf') as file:
 for k, v in fields.items():
     with open(os.path.join(FOLDER, f'{k}.txt.data'), 'w') as file:
         file.write('\n'.join(sorted(v)))
+    common_prefix: str = find_common_prefix(v)
+    if common_prefix is not None:
+        stripped = sorted([strip_prefix(x, common_prefix) for x in v])
+        with open(os.path.join(FOLDER, f'{k}_stripped.txt.data'), 'w') as file:
+            file.write('\n'.join([f'prefix: {common_prefix}', *stripped]))
 print(time_since(program_start))
