237af3fa3dfbcfa7559b0a88bcfcf644e1d7096a
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-10-26 14:52:21 -0600

    some printing improvements and todo notes

--- scripts/Decks/DivisionCostMatrix/costmatrixutility.py
+++ scripts/Decks/DivisionCostMatrix/costmatrixutility.py
@@ -5,7 +5,8 @@ from ndf_parse import Mod
 from ndf_parse.model import List, ListRow, Map, MapRow
 
 CONSECUTIVE_CELL_DISCOUNT = 0.9
-PADDING = len('Infantry') + 2
+CATEGORY_PADDING = len('Infantry') + 2
+DIVISION_NAME_PADDING = len('Unternehmen Zentrum') + 1
 
 def marginal_utility(cell_index: int) -> float:
     return math.pow(CONSECUTIVE_CELL_DISCOUNT, cell_index)
@@ -40,7 +41,7 @@ class CostMatrixRow(object):
     @property
     def utility(self: Self) -> float:
         result: float = 0
-        for i in len(self):
+        for i in range(len(self)):
             result += marginal_utility(i) * cost_utility(self[i])
         return result
     
@@ -63,15 +64,17 @@ class CostMatrix(object):
         return self._rows[key]
     
     def __str__(self: Self) -> str:
-        return f'{self.name}\n\t{'\n\t'.join(f'{k.ljust(PADDING)} {str(v)}' for k, v in self)}'
+        return f'{self.name}\n\t{'\n\t'.join(f'{k.ljust(CATEGORY_PADDING)} {str(v)}' for k, v in self)}'
 
     @property
-    def total_utility(self: Self) -> float:
-        return sum(v.utility for _, v in self)
+    def total_utility(self: Self, places: int = 2) -> float:
+        return round(sum(v.utility for _, v in self), places)
     
     @staticmethod
     def from_ndf(row: ListRow) -> Self:
-        return CostMatrix(' '.join(row.namespace.split('_')[2:-1]), **{x.key.split('/')[-1]: CostMatrixRow.from_ndf(x.value) for x in row.value})
+        result: CostMatrix = CostMatrix(' '.join(row.namespace.split('_')[2:-1]), **{x.key.split('/')[-1]: CostMatrixRow.from_ndf(x.value) for x in row.value})
+        del result._rows['Defense']
+        return result 
     
 MOD_PATH = rf'C:\Program Files (x86)\Steam\steamapps\common\WARNO\Mods\default'
 GENERATED_PATH = MOD_PATH # rf'{MOD_PATH}\GameData\Generated'
@@ -80,6 +83,24 @@ mod = Mod(MOD_PATH, MOD_PATH)
 
 FOLDER = 'Decks/DivisionCostMatrix'
 
+matrices: list[CostMatrix] = []
+
 with mod.edit('GameData/Generated/Gameplay/Decks/DivisionCostMatrix.ndf') as file:
     for row in file:
-        print(str(CostMatrix.from_ndf(row)))
+        if row.namespace.endswith('_multi'):
+            matrices.append(CostMatrix.from_ndf(row))
+
+for matrix in sorted(matrices, key=lambda x: x.total_utility, reverse=True):
+    print(f'{f'{matrix.name}:'.ljust(DIVISION_NAME_PADDING)} {matrix.total_utility}')
+
+def median(*fs: float) -> float:
+    fs = sorted(fs)
+    half = len(fs) // 2
+    if len(fs) % 2 == 0:
+        return (fs[half] + fs[half + 1]) / 2
+    else:
+        return fs[half]
+    
+print(f'\n{f'Median:'.ljust(DIVISION_NAME_PADDING)} {median(*[x.total_utility for x in matrices])}')
+# todo: calculate standard deviation
+# todo: show utility distributions by category
